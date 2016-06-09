6989c2b8f2097abaac123110f186a2a38a6dce52
SabriFIUBA <scortinnas@gmail.com>
2016-06-09 13:13:55 -0300

    Create paridad.cpp

---
+++ paridad/paridad.cpp
@@ -0,0 +1,17 @@
+#include "esPar.h"
+#include "esImpar.h"
+
+int main(){
+
+    int n;
+    cout << "Ingrese un numero entero:  ";
+    cin >> n; cout << endl << endl;
+
+
+    esPar(n);
+
+
+
+
+return 0;
+}
